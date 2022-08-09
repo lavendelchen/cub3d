a79ef6f5648c524a2c697d5521fa84db80ab9cce
toni-schmitt <tonischmitt@online.de>
2022-08-09 17:32:59 +0200

    REFACTOR (test_maps.sh): Now Script only Creates Folders and did_not_pass.log on Error - Also now Outputs Error to STDOUT - Also remove unused Color-Variables
    Signed-off-by: toni-schmitt <tonischmitt@online.de>

--- test_maps.sh
+++ test_maps.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-
-BOLD="\e[1m"
-RED="\e[41m"
-GREEN="\e[32m"
-RESET="\e[0m"
-
 ERROR_LOG="did_not_pass.log"
 ERROR_DIR="./did_not_pass"
 
 output=""
 
 rm -rf $ERROR_DIR
-mkdir $ERROR_DIR
 rm -rf $ERROR_LOG
-touch $ERROR_LOG
 
 for valid_map in ./scene_descriptions/valid/*; do
 	if ! output=$( (./cub3d "$valid_map") 2>&1); then
-    mkdir -p "$ERROR_DIR/valid"
 	  echo "$valid_map got flagged as invalid"
+	  # Copy Valid Map that got flagged as invalid to $ERROR_DIR
+    mkdir -p "$ERROR_DIR/valid"
 	  cp "$valid_map" "$ERROR_DIR/valid/"
+	  # Print Error Output to $ERROR_LOG
 	  printf "%s\n%s\n" "$valid_map" "$output" >> $ERROR_LOG
+	  # Print Error Output also to STDOUT
+	  printf "%s\n%s\n" "./cub3d $valid_map" "$output"
 	fi
 done
 
 for invalid_map in ./scene_descriptions/invalid/*; do
 	if output=$( (./cub3d "$invalid_map") 2>&1); then
 	  echo "$invalid_map got flagged as valid"
+	  # Copy Invalid Map that got flagged as valid to $ERROR_DIR
     mkdir -p "$ERROR_DIR/invalid"
 	  cp "$invalid_map" "$ERROR_DIR/invalid/"
+	  # Print Error Output to $ERROR_LOG
 	  printf "%s\n%s\n" "$invalid_map" "$output" >> $ERROR_LOG
+	  # Print Error Output also to STDOUT
+	  printf "%s\n%s\n" "./cub3d $invalid_map" "$output"
 	fi
 done
